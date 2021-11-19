b0123f5c3a9c72789e5f0384493c23d0fad9fb9d
BryantCR <bryancvargas20@gmail.com>
2021-11-18 19:11:37 -0600

    ok

--- app.js
+++ app.js
@@ -1,44 +1,31 @@
-let express = require("express");
-let session = require('express-session');
-let path = require('path');
-let app = express();
-let bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "./static")));
-app.use(session({secret: 'codingDojo'}));
+var express = require("express");
+var session = require("express-session");
+var app = express();
 
-app.set('views', path.join(__dirname, './views'));
-app.set('view engine', 'ejs');
+app.use(session({
+    secret: 'root',
+}))
 
-app.get('/', function(req, res) {
-    if(!req.session['counter']){
-        req.session['counter'] = 1;
-    } 
-    else{
-        req.session['counter'] +=1;
+app.use(express.static(__dirname + "/static"));
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    if(req.session.counter == null){
+        req.session.counter = 0;
     }
-    res.render("index", {'counter' : req.session.counter});
-})
-
-app.get('/add2', function(req, res) {
-    if(!req.session['counter']){
-        req.session['counter'] = 1;
-    } 
-    else{
-        req.session['counter'] +=1;
+    else {
+        req.session.counter += 1;
     }
-        res.redirect('/');
+    res.render("index", {counter : req.session.counter});
 })
 
-app.get('/reset', function(req, res) {
-        req.session['counter'] = 0;
-    res.redirect('/');
+app.get("/plus", function(req, res){
+    req.session.counter += 1;
+    res.redirect("/");
 })
 
-
-
-
-// tell the express app to listen on port 8000
-app.listen(8080, function() {
- console.log("listening on port 8080");
-});
+app.listen(8000, function(){
+    console.log("Listening on port: 8000");
+})
